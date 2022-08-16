3ef80a8669984dd17aedaaacabedce4d6e1508b6
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2022-08-16 01:31:51 -0700

    Initial Commit

---
+++ muse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  muse
+//
+//  Created by Dante Gil-Marin on 8/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ muse/museApp.swift
@@ -0,0 +1,17 @@
+//
+//  museApp.swift
+//  muse
+//
+//  Created by Dante Gil-Marin on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct museApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
