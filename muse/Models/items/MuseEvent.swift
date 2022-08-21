126427cde8c5a7e2113f8cd87e1c7ec736be4cee
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2022-08-20 19:38:12 -0700

    added habit and changed event to reflect that

--- muse/Models/items/MuseEvent.swift
+++ muse/Models/items/MuseEvent.swift
@@ -8,41 +8,41 @@
 import Foundation
 
 class MuseEvent: MuseItem {
-  var startTime: Date
-  var endTime: Date
+  var startDateTime: Date
+  var endDateTime: Date
   
   private enum MuseEventError: Error {
-    case invalidTime
+    case invalidDateTime
   }
   
   private enum CodingKeys: String, CodingKey {
-    case startTime
-    case endTime
+    case startDateTime
+    case endDateTime
   }
   
   override func encode(to encoder: Encoder) throws {
     try super.encode(to: encoder)
     
     var container = encoder.container(keyedBy: CodingKeys.self)
-    try container.encode(startTime, forKey: .startTime)
-    try container.encode(endTime, forKey: .endTime)
+    try container.encode(startDateTime, forKey: .startDateTime)
+    try container.encode(endDateTime, forKey: .endDateTime)
   }
   
-  init(summary: String, starttime: Date, endtime: Date, customStamp: Date? = nil) throws {
-    guard starttime < endtime else {
-      throw MuseEventError.invalidTime
+  init(summary: String, startDateTime: Date, endDateTime: Date, customStamp: Date? = nil) throws {
+    guard startDateTime < endDateTime else {
+      throw MuseEventError.invalidDateTime
     }
     
-    self.startTime = starttime
-    self.endTime = endtime
+    self.startDateTime = startDateTime
+    self.endDateTime = endDateTime
     
     super.init(summary: summary, customStamp: customStamp)
   }
   
   required init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
-    startTime = try container.decode(Date.self, forKey: .startTime)
-    endTime = try container.decode(Date.self, forKey: .endTime)
+    startDateTime = try container.decode(Date.self, forKey: .startDateTime)
+    endDateTime = try container.decode(Date.self, forKey: .endDateTime)
     
     try super.init(from: decoder)
   }
@@ -52,17 +52,17 @@ extension MuseEvent {
   static var data: [MuseEvent] {
     [
       (try? MuseEvent(summary: "event test 1",
-                starttime: Date(timeIntervalSinceNow: 864000),
-                endtime: Date(timeIntervalSinceNow: 864000+3600)))!,
+                startDateTime: Date(timeIntervalSinceNow: 864000),
+                endDateTime: Date(timeIntervalSinceNow: 864000+3600)))!,
       (try? MuseEvent(summary: "event test 2",
-                starttime: Date(timeIntervalSinceNow: 864000+3600),
-                endtime: Date(timeIntervalSinceNow: 864000+3600*2)))!,
+                startDateTime: Date(timeIntervalSinceNow: 864000+3600),
+                endDateTime: Date(timeIntervalSinceNow: 864000+3600*2)))!,
       (try? MuseEvent(summary: "event test 3",
-                starttime: Date(timeIntervalSinceNow: 864000*3),
-                endtime: Date(timeIntervalSinceNow: 864000*3+3600)))!,
+                startDateTime: Date(timeIntervalSinceNow: 864000*3),
+                endDateTime: Date(timeIntervalSinceNow: 864000*3+3600)))!,
       (try? MuseEvent(summary: "event test 4",
-                starttime: Date(timeIntervalSinceNow: 864000*7),
-                endtime: Date(timeIntervalSinceNow: 864000*7+3600)))!
+                startDateTime: Date(timeIntervalSinceNow: 864000*7),
+                endDateTime: Date(timeIntervalSinceNow: 864000*7+3600)))!
     ]
   }
 }
